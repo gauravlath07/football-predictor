9a379548eb964d822a2ba79dd45c79945df25d81
Chenlei Shen <shenchenlei@gmail.com>
2017-04-23 03:09:44 -0400

    get all default players in one go

--- api/defaultplayers.py
+++ api/defaultplayers.py
@@ -10,11 +10,19 @@ class defaultplayers(object):
         self.main_11 = {}
 
     def on_get(self, req, resp):
+        with open('indexing/utilities/main-11.csv') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                self.main_11[row['team_name']] = row['players'].strip()
+        defaultplayers = self.main_11
+        # defaultplayers['team1_players']=self.main_11[home]
+        # defaultplayers['team2_players'] = self.main_11[away]
+
+        resp.status = falcon.HTTP_201
         msg = {
-            'score': '100!'
+            'defaultplayers': defaultplayers
         }
         resp.body = json.dumps(msg)
-        resp.status = falcon.HTTP_200
 
     def on_post(self, req, resp):
         data = req.stream.read(req.content_length or 0)
